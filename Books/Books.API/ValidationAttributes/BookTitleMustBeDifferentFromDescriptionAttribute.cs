ebd3aa6bc21494bef3c3efd954bea2b7e735a1b0
Darren B <39957945+DB-200@users.noreply.github.com>
2020-08-12 15:57:16 +0100

    Add files via upload

---
+++ Books/Books.API/ValidationAttributes/BookTitleMustBeDifferentFromDescriptionAttribute.cs
@@ -0,0 +1,29 @@
+﻿using Books.API.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Books.API.ValidationAttributes
+{
+    public class BookTitleMustBeDifferentFromDescriptionAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var book = (BookForCreationDto)validationContext.ObjectInstance;
+            if (book.Title.Trim().ToUpper() == book.Description.Trim().ToUpper())
+            {
+                return new ValidationResult(ErrorMessage,
+                                                   new[] { nameof(BookForCreationDto) });
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
+
+
+
+
+
